da5404e2cc1f1f44488084d961b184a12340ac4f
Nicholas <appledev.booker@gmail.com>
2022-05-12 12:50:48 +0900

    feat: 프린트 함수 구현

--- testPassOrder/ViewController.swift
+++ testPassOrder/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("nick이 구현했습니다.")
     }
 
 
